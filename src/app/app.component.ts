a4e769b956e2d6d410366c06e850655fec50acda
ivancostilla <ivan20punto88@gmail.com>
2021-07-06 18:03:17 -0300

    componente cabecero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { Egreso } from './egreso/egreso.model';
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/ingreso.model';
+import { IngresoServicio } from './ingreso/ingreso.servicio';
+import { EgresoServicios } from './egreso/egreso.servicio';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presupuesto-app';
+
+  ingresos:Ingreso[]=[];
+  egresos:Egreso[]=[];
+
+  constructor(
+    //traemos la info del servicio
+    private ingresoServicio:IngresoServicio,
+    private egresoServicio:EgresoServicios
+    ){
+      //variable que traemos del servicio
+      //y la guardamos en la variable que creamos acá
+      this.ingresos = ingresoServicio.ingresos;
+      this.egresos = egresoServicio.egresos;
+    }
+
+    //obtenemos la info del ingreso total
+    getIngresoTotal(){
+      //nicializo variable para guardar el ingreso total
+      let ingresoTotal:number=0;
+      this.ingresos.map(ingreso => {
+        /* x cada ingreso dentro del array 'ingresos'
+        sumamos el valor a la variable ingreso total
+         */
+        ingresoTotal =+ ingreso.valor;
+      })
+      return ingresoTotal;
+    };
+
+    //obtenemos la info del egreso total
+    getEgresoTotal(){
+      //nicializo variable para guardar el ingreso total
+      let egresoTotal:number=0;
+      this.egresos.map(egreso => {
+        /* x cada ingreso dentro del array 'ingresos'
+        sumamos el valor a la variable ingreso total
+         */
+        egresoTotal += egreso.valor;
+      })
+      return egresoTotal;
+    };
+
+    //metodo para obtener el porcentaje
+    getPorcentajeTotal(){
+      return this.getEgresoTotal() / this.getIngresoTotal();
+    }
+
+    //presupuesto total
+    getPresupuestoTotal(){
+      return this.getIngresoTotal() - this.getEgresoTotal();
+    }
 }
--- src/app/ingreso/ingreso.servicio.ts
+++ src/app/ingreso/ingreso.servicio.ts
@@ -6,6 +6,6 @@ export class IngresoServicio{
   ingresos:Ingreso[]=[
     //acá usamos el model.ts:
     new Ingreso('Salario',4000),
-    new Ingreso('Venta de Coche', 500)
+    new Ingreso('Venta de Coche', 4500)
   ];
 }
