4dbf73bde0cad786fade6f05d2e4169ca3c1cd77
ivancostilla <ivan20punto88@gmail.com>
2021-07-06 17:31:35 -0300

    componentes, model.ts y servicios

---
+++ src/app/ingreso/ingreso.servicio.ts
@@ -0,0 +1,11 @@
+import { Ingreso } from "./ingreso.model";
+
+export class IngresoServicio{
+
+  //inicializamos una variable con el objeto Ingreso:
+  ingresos:Ingreso[]=[
+    //acá usamos el model.ts:
+    new Ingreso('Salario',4000),
+    new Ingreso('Venta de Coche', 500)
+  ];
+}
